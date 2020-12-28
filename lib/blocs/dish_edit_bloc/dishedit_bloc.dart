59b645a8002becddd2a79764b3378a4e53bacd93
Sakorni <ssakorni@gmail.com>
2020-12-28 13:24:21 +0300

    WIP abs edit bloc

--- lib/blocs/dish_edit_bloc/dishedit_bloc.dart
+++ lib/blocs/dish_edit_bloc/dishedit_bloc.dart
@@ -8,12 +8,25 @@ part 'dishedit_event.dart';
 part 'dishedit_state.dart';
 
 class DisheditBloc extends Bloc<DisheditEvent, DisheditState> {
-  DisheditBloc() : super(DisheditInitial());
+  Dish _dish;
+  DisheditBloc(this._dish) : super(DishEditMainState(_dish));
 
   @override
   Stream<DisheditState> mapEventToState(
     DisheditEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    bool successful = false;
+    if (event is EditingDone) {
+      yield DishEditLoadingState();
+      try {
+        //TODO: Implement actions
+        _dish = (event as EditingDone).dish;
+        successful = true;
+      } on Exception {
+        print('wooob');
+      }
+      yield EditingComplete(successful);
+      yield DishEditMainState(_dish);
+    }
   }
 }
--- lib/blocs/dish_edit_bloc/dishedit_state.dart
+++ lib/blocs/dish_edit_bloc/dishedit_state.dart
@@ -3,4 +3,15 @@ part of 'dishedit_bloc.dart';
 @immutable
 abstract class DisheditState {}
 
-class DisheditInitial extends DisheditState {}
+class DishEditMainState extends DisheditState {
+  final Dish dish;
+  DishEditMainState(this.dish);
+}
+
+class EditingComplete extends DisheditState {
+  final bool successful;
+
+  EditingComplete(this.successful);
+}
+
+class DishEditLoadingState extends DisheditState {}
