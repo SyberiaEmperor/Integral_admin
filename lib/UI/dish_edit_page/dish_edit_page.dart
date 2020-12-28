c77eb91255cd436d65a99b0a00807cc00345b669
Sakorni <ssakorni@gmail.com>
2020-12-28 13:42:44 +0300

    Reworked DEP

--- lib/UI/dish_edit_page/dish_edit_page.dart
+++ lib/UI/dish_edit_page/dish_edit_page.dart
@@ -3,14 +3,19 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:integral_admin/UI/dish_edit_page/widgets/button_bar.dart';
+import 'package:integral_admin/blocs/dish_edit_bloc/dishedit_bloc.dart';
 
 import 'package:integral_admin/entities/dish.dart';
 import 'package:integral_admin/services/images.dart';
 import 'package:integral_admin/services/responsive_size.dart';
 import 'package:integral_admin/UI/widgets/tag_controller/tag_controller.dart';
 
+import 'widgets/picture_and_price.dart';
+
 class DishEditScreen extends StatelessWidget {
   final Dish _dish;
+  DisheditBloc bloc;
 
   final TextEditingController price = TextEditingController();
   final TextEditingController descpription = TextEditingController();
@@ -24,6 +29,7 @@ class DishEditScreen extends StatelessWidget {
     descpription.text = _dish.description;
     name.text = _dish.name;
     cats = Set.from(_dish.categoriesSet);
+    bloc = DisheditBloc(_dish);
   }
   @override
   Widget build(BuildContext context) {
@@ -71,11 +77,14 @@ class DishEditScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20.height),
-                  _PictureAndPrice(
+                  PictureAndPrice(
                     priceController: price,
                     dishPrice: _dish.price,
                     picUrl: _dish.url,
                     img: image,
+                    picChanged: (newPic) {
+                      image = newPic;
+                    },
                   ),
                   SizedBox(height: 20.height),
                   Text(
@@ -105,146 +114,10 @@ class DishEditScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: _ButtonBar(
+      bottomNavigationBar: BottomButtonBar(
         leftFieldCallback: () => print('left'),
         rightFieldCallback: () => print('Right'),
       ),
-    );
-  }
-}
-
-class _PictureAndPrice extends StatefulWidget {
-  final String picUrl;
-  final int dishPrice;
-
-  final TextEditingController priceController;
-  Uint8List img;
-
-  _PictureAndPrice({
-    @required this.picUrl,
-    @required this.dishPrice,
-    this.priceController,
-    this.img,
-  });
-
-  @override
-  _PictureAndPriceState createState() => _PictureAndPriceState();
-}
-
-class _PictureAndPriceState extends State<_PictureAndPrice> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      GestureDetector(
-        onTap: () async {
-          widget.img = await Images.getImage();
-          setState(() {});
-        },
-        child: Container(
-          width: double.infinity,
-          height: 281.height,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: widget.img == null
-                  ? (widget.picUrl == null
-                      ? AssetImage('asset/no_image.png')
-                      : CachedNetworkImageProvider(widget.picUrl))
-                  : MemoryImage(widget.img),
-            ),
-          ),
-        ),
-      ),
-      Container(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-        decoration: BoxDecoration(
-          color: Theme.of(context).primaryColor,
-          border: Border.all(color: Colors.blueGrey[100], width: 0),
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Цена:', style: Theme.of(context).primaryTextTheme.bodyText1),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              height: 75,
-              width: 200,
-              child: TextField(
-                style: Theme.of(context).primaryTextTheme.bodyText1,
-                controller: widget.priceController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)),
-                  suffix: Text('Руб.',
-                      style: Theme.of(context).primaryTextTheme.bodyText1),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ]);
-  }
-}
-
-class _ButtonBar extends StatelessWidget {
-  final void Function() leftFieldCallback;
-  final void Function() rightFieldCallback;
-
-  const _ButtonBar(
-      {@required this.leftFieldCallback, @required this.rightFieldCallback});
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Padding(
-          padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
-          child: GestureDetector(
-            onTap: null,
-            child: Container(
-                height: ResponsiveSize.height(70),
-                width: ResponsiveSize.width(60),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Color(0xff0C2944),
-                ),
-                child: Icon(
-                  Icons.delete,
-                  size: 20,
-                  color: Colors.white,
-                )),
-          ),
-        ),
-        Container(
-          width: 200,
-          child: GestureDetector(
-            onTap: null,
-            child: Container(
-                height: ResponsiveSize.height(70),
-                width: ResponsiveSize.width(60),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      bottomLeft: Radius.circular(15)),
-                  color: Theme.of(context).accentColor,
-                ),
-                child: Icon(
-                  Icons.check,
-                  size: 20,
-                  color: Colors.white,
-                )),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/UI/dish_edit_page/widgets/button_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:integral_admin/services/responsive_size.dart';
+
+class BottomButtonBar extends StatelessWidget {
+  final void Function() leftFieldCallback;
+  final void Function() rightFieldCallback;
+
+  const BottomButtonBar(
+      {@required this.leftFieldCallback, @required this.rightFieldCallback});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
+          child: GestureDetector(
+            onTap: leftFieldCallback,
+            child: Container(
+                height: ResponsiveSize.height(70),
+                width: ResponsiveSize.width(60),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color(0xff0C2944),
+                ),
+                child: Icon(
+                  Icons.delete,
+                  size: 20,
+                  color: Colors.white,
+                )),
+          ),
+        ),
+        Container(
+          width: 200,
+          child: GestureDetector(
+            onTap: rightFieldCallback,
+            child: Container(
+                height: ResponsiveSize.height(70),
+                width: ResponsiveSize.width(60),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      bottomLeft: Radius.circular(15)),
+                  color: Theme.of(context).accentColor,
+                ),
+                child: Icon(
+                  Icons.check,
+                  size: 20,
+                  color: Colors.white,
+                )),
+          ),
+        ),
+      ],
+    );
+  }
+}
