5ab2bb72cca4aca0b338e6935c3fc1b99648337d
SyberiaImperor <Al.B.danilenko@gmail.com>
2021-02-07 13:24:01 +0300

    user_repository.dart добавлен

--- lib/UI/auth_page/auth_page.dart
+++ lib/UI/auth_page/auth_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:integral_admin/UI/auth_page/widgets/auth_title.dart';
 import 'package:integral_admin/UI/auth_page/widgets/input_field.dart';
+import 'package:integral_admin/blocs/auth_bloc/auth_bloc.dart';
 import 'package:integral_admin/services/responsive_size.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,35 +13,40 @@ class AuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AuthTitle(),
-          InputField.emailField(email),
-          InputField.passwordField(password),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                width: ResponsiveSize.width(100),
-                height: ResponsiveSize.height(50),
-                decoration: BoxDecoration(
-                    color: Theme.of(context).accentColor,
-                    borderRadius:
-                        BorderRadius.circular(ResponsiveSize.height(10))),
-                child: Text(
-                  'Войти',
-                  style: Theme.of(context)
-                      .primaryTextTheme
-                      .bodyText2
-                      .copyWith(color: Colors.white),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AuthTitle(),
+              InputField.emailField(email),
+              InputField.passwordField(password),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: ResponsiveSize.width(100),
+                    height: ResponsiveSize.height(50),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius:
+                            BorderRadius.circular(ResponsiveSize.height(10))),
+                    child: Text(
+                      'Войти',
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .bodyText2
+                          .copyWith(color: Colors.white),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
---
+++ lib/entities/user_repository.dart
@@ -0,0 +1,11 @@
+import 'user.dart';
+
+class UserRepository {
+  static User _user;
+
+  static void addUser(User user) {}
+
+  static User user() {
+    return user();
+  }
+}
